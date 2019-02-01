1c0cbca15577d8341fc09ae6a3619b2dff0f5992
Hackintosh 5 <penn.mackintosh@gmail.com>
2019-02-01 08:35:44 +0000

    WIP remove tabs etc

--- app/src/main/java/net/wildfyre/client/android/CardLayoutManager.kt
+++ app/src/main/java/net/wildfyre/client/android/CardLayoutManager.kt
@@ -1,39 +1,31 @@
 package net.wildfyre.client.android
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.wildcard.view.*
 
-class CardAdapter(val text: String, var c: Int) : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
+class CardAdapter() : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
     private var context: Context? = null
-    public var count: Int = 0
-
-    init {
-        count = c
-    }
 
     override fun getItemCount(): Int {return 1} //We only have one card per page
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         assert(position == 0)
-        Log.e("CardAdapter", count.toString())
-        holder.text.text = text + count
+        holder.text.text = /*context?.getString(R.string.)*/""
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
         var layoutInflater = LayoutInflater.from(context)
         var cardView = layoutInflater.inflate(R.layout.wildcard, parent, false)
-        return ViewHolder(cardView, count)
+        return ViewHolder(cardView)
     }
 
-    class ViewHolder(itemView: View, var count: Int) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var text: TextView
         init {
             text = itemView.section_label
--- app/src/main/java/net/wildfyre/client/android/MainActivity.kt
+++ app/src/main/java/net/wildfyre/client/android/MainActivity.kt
@@ -1,56 +1,42 @@
 package net.wildfyre.client.android
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
-
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_main.view.*
+
+import net.wildfyre.api.WildFyre
+import net.wildfyre.areas.*
+import net.wildfyre.posts.*
 
 class MainActivity : AppCompatActivity() {
-
-    /**
-     * The [androidx.viewpager.widget.PagerAdapter] that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * androidx.fragment.app.FragmentStatePagerAdapter.
-     */
-    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
-
-        // Set up the ViewPager with the sections adapter.
-        container.adapter = mSectionsPagerAdapter
 
         fab.setOnClickListener {}
 
-    }
 
+        wildcardlist.adapter = CardAdapter()
+        wildcardlist.layoutManager = LinearLayoutManager(this)
+        val helper = ItemTouchHelper(CardSwipeCallback(this, wildcardlist))
+        helper.attachToRecyclerView(wildcardlist)
+
+
+        // Test code
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-        return false
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -59,72 +45,11 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
 
-//        if (id == R.id.action_settings) {
-//            return true
-//        }
+        if (id == R.id.action_settings) {
+            return true
+        }
 
         return super.onOptionsItemSelected(item)
     }
 
-
-    /**
-     * A [FragmentPagerAdapter] that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-
-        override fun getItem(position: Int): Fragment {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a WildfyreFragment (defined as a static inner class below).
-            return WildfyreFragment.newInstance(when (position) {
-                0 -> getString(R.string.dashboard)
-                1 -> getString(R.string.home)
-                2 -> getString(R.string.notifications)
-                else -> position.toString()
-            })
-        }
-
-        override fun getCount(): Int {
-            // Show 3 total pages.
-            return 3
-        }
-    }
-
-    /**
-     * A fragment
-     */
-    class WildfyreFragment : Fragment() {
-
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-            rootView.wildcardlist.adapter = CardAdapter(arguments!!.getString(ARG_SECTION_NAME, "error"), 0)
-            rootView.wildcardlist.layoutManager = LinearLayoutManager(container?.context)
-            val helper = ItemTouchHelper(CardSwipeCallback(context!!, rootView.wildcardlist))
-            helper.attachToRecyclerView(rootView.wildcardlist)
-            return rootView
-        }
-
-        companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
-            private const val ARG_SECTION_NAME = "section_name"
-
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
-            fun newInstance(sectionName: String): WildfyreFragment {
-                val fragment = WildfyreFragment()
-                val args = Bundle()
-                args.putString(ARG_SECTION_NAME, sectionName)
-                fragment.arguments = args
-                return fragment
-            }
-        }
-    }
 }
